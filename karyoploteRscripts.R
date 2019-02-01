2414afd5ca8ee25726d16768e81d13846f024d86
BCI-Barts <47106172+BCI-Barts@users.noreply.github.com>
2019-02-01 10:06:37 +0000

    Add files via upload

--- karyoploteRscripts.R
+++ karyoploteRscripts.R
@@ -1,7 +1,19 @@
 library("karyoploteR")
 #--------------------------------------------------------
+d<-read.table("C://Users/lobley01/Documents/ctDNA_PROJECT/test.txt",
+              header=T)
 gmax<-2
 chrs<-"chr9"
+
+gains<-d[which(d$log2>0),]
+losses<-d[which(d$log2<0),]
+
+
+data.ranges<-GRanges(chr=d$chr,
+                     IRanges(start=d$start,end=d$end),
+                     seqnames=d$chr,genes=d$gene,
+                     logR=d$log2,pvalue=d$pvalue)
+
 gains.genes<-unique(gains$gene[which(gains$pvalue<0.05)])
 gains.genes.data<-data.ranges[match(gains.genes,d$gene)]
 loss.genes<-unique(losses$gene[which(losses$pvalue<0.05)])
@@ -37,28 +49,7 @@ kpAddBaseNumbers(kp,
 #--------------------------------------------------------
 kpAddCytobandLabels(kp)
 #--------------------------------------------------------
-#show losses and gains
-
 #show signif genes only i.e. zoom in on regions
+#--------------------------------------------------------
 
-
-#data <- toGRanges(data.frame(chr="chr1", start=10000000*(0:23), end=10000000*(1:24)))
-
-#kp <- plotKaryotype("hg19", plot.type=2, chromosomes=c("chr1", "chr2"))
-
-#We can specify all data values separately. If missing y0, it defaults to ymin
-#kpBars(kp, chr=as.character(seqnames(data)), x0=start(data), x1=end(data), y1=y1,
-#       col="#FFBBBB", border="#EEAAAA")
-#kpLines(kp, data=data, y=y1, col="red")
-
-#or we can provide all data into a single GRanges object
-#mcols(data) <- data.frame(y0=y0, y1=y1)
-#kpBars(kp, data[data$y0>data$y1], col="orange", border="orange", data.panel=2)
-#kpBars(kp, data[data$y0<=data$y1], col="purple", border="purple", data.panel=2)
-
-#kpLines(kp, data, y=data$y1, data.panel=2, col="red")
-#kpLines(kp, data, y=data$y0, data.panel=2, col="blue")
-
-#kpAxis(kp, data.panel = 1, cex=0.8, numticks = 5, col="#777777")
-#kpAxis(kp, data.panel = 2, cex=0.8, numticks = 5, col="#777777")
 
