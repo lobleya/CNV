4cac0173bffe55b206674b486284c96c64bc30c9
BCI-Barts <47106172+BCI-Barts@users.noreply.github.com>
2019-02-19 12:43:39 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
+module load samtools
+module load bcftools
+
+
 export tumor=$1
 export normal=$2
 export genome=$3
+
 #  ../bams/WGS/B01P0095AAA03_tumour.bam  ../bams/WGS/B01P0095_ABC03_normal.bam
-#   ln -s --force $normal Normal.bam
-#   ln -s --force $tumor Tumour.bam
-   TARGET="interval-exome.target-267.bed"
-   ACCESS="hg38_access2k.bed"
-   OUT=`echo $tumor | sed s/.bam//g`
-   OUT=`basename $OUT`
-   echo "$OUT $ACCESS $TARGET"
+
+TARGET="interval-exome.target-267.bed"
+ACCESS="hg38_access2k.bed"
+OUT=`echo $tumor | sed s/.bam//g`
+OUT=`basename $OUT`
+
+echo "$OUT $ACCESS $TARGET"
 
 `date`
 
-python3 cnvkit.py batch tumour.bam --normal normal.bam \
+python3 cnvkit.py batch $1 --normal $2 \
     --fasta $genome --access data/$ACCESS  \
     -t data/$TARGET \
     --output-reference my_reference.cnn --output-dir results/$OUT \
-    --diagram --scatter
+
  
  `date`
