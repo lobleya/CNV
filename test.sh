d53a52e23e66a8cf2524f0d5505c584d41984998
Anna Lobley <47106172+BCI-Barts@users.noreply.github.com>
2019-11-06 11:00:15 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,12 +9,15 @@ export normal=$2
 export genome=$3
 
 #  ../bams/WGS/B01P0095AAA03_tumour.bam  ../bams/WGS/B01P0095_ABC03_normal.bam
-
+#--- selected by plotting these data carefully using the fragment sizes
+#-----------------------------------------
 TARGET="interval-exome.target-267.bed"
 ACCESS="hg38_access2k.bed"
 OUT=`echo $tumor | sed s/.bam//g`
 OUT=`basename $OUT`
-
+#-----------------------------------------
+# Note that we need a pool of normals for CP
+#-----------------------------------------
 echo "$OUT $ACCESS $TARGET"
 
 `date`
@@ -22,7 +25,9 @@ echo "$OUT $ACCESS $TARGET"
 python3 cnvkit.py batch $1 --normal $2 \
     --fasta $genome --access data/$ACCESS  \
     -t data/$TARGET \
-    --output-reference my_reference.cnn --output-dir results/$OUT \
+    --output-reference $1.reference.cnn \
+    --output-dir `basename $TUMOUR` \
 
  
  `date`
+ #-------------------------------------------
